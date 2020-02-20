c2ab23dfefe711106a8541fc23fcb24463f26be0
Aurelien Joncour <aurelien.joncour@epitech.eu>
2020-02-20 13:39:59 +0100

    fix(game.c) changing condition for wrong attack

--- src/game.c
+++ src/game.c
@@ -52,15 +52,18 @@ bool write_attack(vector_t *v)
         if (str == NULL)
             break;
         if (my_strlen(str) != 2) {
+            free(str);
             my_putstr("wrong position\n");
             continue;
         }
         v->x = POS_X(str[0]);
         v->y = POS_Y(str[1]);
-        if (v->y > MAP_SIZE || v->x > MAP_SIZE) {
+        if (v->y >= MAP_SIZE || v->x >= MAP_SIZE) {
+            free(str);
             my_putstr("wrong position\n");
             continue;
         }
+        free(str);
         return true;
     }
     return false;
