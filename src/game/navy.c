1d337d12422b59290ab9578c6030bb05849dc031
RACAUD Simon <simonracaud@gmail.com>
2020-02-24 11:46:52 +0100

    fix(standard error) : navy function => remove comment

--- src/game/navy.c
+++ src/game/navy.c
@@ -19,12 +19,6 @@ int navy(char **game_map, pid_t enemy_pid)
         my_putstr_error("navy : invalid game map\n");
         return EXIT_ERROR;
     }
-    /*
-    if (s.signal_usr2 == 1)
-        return player_2(map_enemy, game_map);
-    else
-        return player_1(map_enemy, game_map);
-    */
     if (s.signal_usr2 == 1)
         return player(map_enemy, game_map, 2);
     else
