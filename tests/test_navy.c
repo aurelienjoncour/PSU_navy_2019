e36dfaa92ca26010c268c3dfb4b855fceee71051
RACAUD Simon <simonracaud@gmail.com>
2020-03-04 09:18:26 +0100

    Fix(Standard error) : too long line in test_navy.c

--- tests/test_navy.c
+++ tests/test_navy.c
@@ -41,8 +41,8 @@ static const char *USAGE =
     "USAGE\n"
     "     ./navy [first_player_pid] navy_positions\n"
     "DESCRIPTION\n"
-    "     first_player_pid:  only for the 2nd player. pid of the first player.\n"
-    "     navy_positions:  file representing the positions of the ships.\n"
+    "     first_player_pid:  only for the 2nd player. pid of the first player"
+    ".\n     navy_positions:  file representing the positions of the ships.\n"
 };
 
 void redirect_all_stdout(void)
